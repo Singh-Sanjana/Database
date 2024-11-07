c54a6d035a904cbaa4738bc98b0646b545fa4ac8
Singh-Sanjana <sanjanasingh26032006@gmail.com>
2024-11-07 12:05:02 +0530

    Add files via upload

---
+++ sqlscript.sql
@@ -0,0 +1,14 @@
+REM   Script: Session 01
+REM   1
+
+create table Department(Dept_no NUMBER,Dept_name Varchar2(10),Deptloc varchar2(10));
+
+insert into Department(Dept_no,Dept_name,Deptloc) 
+values (100,'BCA','BlockA');
+
+insert into Department(Dept_no,Dept_name,Deptloc) 
+values (101,'MCA','BlockB');
+
+insert into Department(Dept_no,Dept_name,Deptloc) 
+values (102,'BBA','BlockC');
+
